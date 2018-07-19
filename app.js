47db1ff909b0bb9685e8d72d2719f356f1b09cfb
Leo espinoza <leo.spinoza@yahoo.com>
2018-07-19 13:57:51 -0500

    added post

--- app.js
+++ app.js
@@ -1,108 +1,109 @@
-var createError = require('http-errors');
 var express = require('express');
 var path = require('path');
-var cookieParser = require('cookie-parser');
+var favicon = require('serve-favicon');
 var logger = require('morgan');
+var cookieParser = require('cookie-parser');
+var bodyParser = require('body-parser');
+var session = require('express-session');
+var multer = require('multer');
+var upload = multer({ dest: 'uploads/' })
+var expressValidator = require('express-validator');
 
-const session = require('express-session')
-const moment = require('moment');
-const multer = require('multer')
-const upload = multer({dest: 'uploads/'})
-const expressValidator = require('express-validator');
-const mongo = require('mongodb');
+var mongo = require('mongodb');
+var db = require('monk')('localhost/blogsite');
 
-// here we are requiring monk by using db as a variable
-// then we are going to use localhost/nodeblog to 
-// tell nodejs that we are going to use the local server
-// and then we are going to have a name nodeblog as the 
-// name of our server
-// NO NEED TO MANUALLY CREATE THE SERVER
-// THE APPLICATION WILL MANUALLY CREATE FOR US
-const db = require('monk')('localhost/nodeblog');
-
-
-
-
-
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var routes = require('./routes/index');
+var posts = require('./routes/posts');
 
 var app = express();
+
+// this is to require the moments locally
+app.locals.moment = require('moment');
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+// uncomment after placing your favicon in /public
+//app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
-app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-// express sessions middleware
+// Express Session
 app.use(session({
-  secret: 'secret',
-  saveUninitialized: true,
-  resave: true
-}))
+    secret: 'secret',
+    saveUninitialized: true,
+    resave: true
+}));
 
-
-// express middleware validator
+// Express Validator
 app.use(expressValidator({
-  errorFormatter: function (param, msg, value) {
-    var namespace = param.split('.'),
-      root = namespace.shift(),
-      formParam = root;
+  errorFormatter: function(param, msg, value) {
+      var namespace = param.split('.')
+      , root    = namespace.shift()
+      , formParam = root;
 
-    while (namespace.length) {
+    while(namespace.length) {
       formParam += '[' + namespace.shift() + ']';
     }
     return {
-      param: formParam,
-      msg: msg,
-      value: value
+      param : formParam,
+      msg   : msg,
+      value : value
     };
   }
 }));
 
 
-// THIS IS TO TELL NODEJS THAT WE ARE GOING TO 
-// USE CONNECT FLASH FOR OUR APPLICATION TO 
-// SHOW MESSAGES 
+// Connect-Flash
 app.use(require('connect-flash')());
-app.use((req, res, next)=>{
+app.use(function (req, res, next) {
   res.locals.messages = require('express-messages')(req, res);
-  next(); 
-})
-
-
-
-// THIS MIDDEWARE IS TO MAKE OUR DATABASE ACCESSIBLE TO 
-// OUR ROUTER
-app.use((req, res, next)=>{
-  req.db = db;
   next();
-})
+});
 
+// Make our db accessible to our router
+app.use(function(req,res,next){
+    req.db = db;
+    next();
+});
 
-
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
+app.use('/', routes);
+app.use('/posts', posts);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
-  next(createError(404));
+  var err = new Error('Not Found');
+  err.status = 404;
+  next(err);
 });
 
-// error handler
+// error handlers
+
+// development error handler
+// will print stacktrace
+if (app.get('env') === 'development') {
+  app.use(function(err, req, res, next) {
+    res.status(err.status || 500);
+    res.render('error', {
+      message: err.message,
+      error: err
+    });
+  });
+}
+
+// production error handler
+// no stacktraces leaked to user
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
   res.status(err.status || 500);
-  res.render('error');
+  res.render('error', {
+    message: err.message,
+    error: {}
+  });
 });
+
 
 module.exports = app;
